fbee19d3097acaf7ce6855e983696284fe45cb16
prabhuIOSDev <prabhuiosdeveloper@gmail.com>
2025-08-13 11:15:30 +0530

    Initial Commit

---
+++ SwiftUIKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIKitDemo
+//
+//  Created by Prabhu Devar on 13/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftUIKitDemoTests/SwiftUIKitDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKitDemoTests.swift
+//  SwiftUIKitDemoTests
+//
+//  Created by Prabhu Devar on 13/08/25.
+//
+
+import Testing
+@testable import SwiftUIKitDemo
+
+struct SwiftUIKitDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
